751e578bd6b393bdda8f83cc6e715fec538e8628
amipatil <iamitpatil1993@gmail.com>
2020-05-01 23:20:08 +0530

    Ribbon: Using Ribbon for client side load balancing using static config

--- src/main/java/org/code4cause/microservices/currencyconversionservice/currencyexchange/FeignCurrencyExchangeProxy.java
+++ src/main/java/org/code4cause/microservices/currencyconversionservice/currencyexchange/FeignCurrencyExchangeProxy.java
@@ -2,6 +2,7 @@ package org.code4cause.microservices.currencyconversionservice.currencyexchange;
 
 import org.code4cause.microservices.currencyconversionservice.dto.SuccessResponse;
 import org.code4cause.microservices.currencyconversionservice.model.CurrencyMapping;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 // spring.application.name here is not important for now, unless we use eureka naming server and ribbon.
 // For now url parameter is only required and usefule
 // Feign client interfaces are declared as  beans in application context with fully qualified name as a bean id
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+
+// this name currently getting used to configure static instance list in application.properties. Later this application
+// name will be used to lookup instance list from Eureka naming server(by spring.application.name) for dynamic load balancing.
+@RibbonClient(name = "currency-exchange-service")
 public interface FeignCurrencyExchangeProxy {
 
     /**
