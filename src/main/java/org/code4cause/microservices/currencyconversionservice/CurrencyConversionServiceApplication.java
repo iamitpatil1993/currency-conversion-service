47c6777e65631bbae0752f766dde4586d5a476fd
amipatil <iamitpatil1993@gmail.com>
2020-05-01 22:15:03 +0530

    Feign-1:Listing Feign client classes manually

--- src/main/java/org/code4cause/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/org/code4cause/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,5 +1,6 @@
 package org.code4cause.microservices.currencyconversionservice;
 
+import org.code4cause.microservices.currencyconversionservice.currencyexchange.FeignCurrencyExchangeProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -7,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 //We need to enable FeignClient before using it. It actually scans classes annotated with @FiengClient
 // we can configure component scanning as usual.
-@EnableFeignClients
+@EnableFeignClients(clients = {FeignCurrencyExchangeProxy.class}) // we can list specific classes as well if we want to reduce scan time and have limited classes
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
